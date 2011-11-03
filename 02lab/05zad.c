8e5d695f1d4710df72041a3515e1aa7a7b7ce454
Your Name <belcik.jakub@gmail.com>
2011-11-03 17:02:59 +0100

    nowe

---
+++ 02lab/05zad.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+  float x;
+
+  scanf("%f", &x);
+
+  int a = (int)(x*100), lewa = a/100, prawa, i, pom, j = 0, k = 0;
+  if (x < 0)
+  {
+    lewa = -lewa;
+    prawa = -a%100;
+  }
+  else prawa = a%100;
+  char L[8], P[2];
+
+  for (i = 1000000000; i != 0; i /= 10)
+  {
+      pom = lewa/i;
+      L[j] = (char)(pom + 48);
+      lewa = lewa - pom*i;
+      j++;
+  }
+
+  for (i = 10; i != 0; i /= 10)
+  {
+    pom = prawa/i;
+    P[k] = (char)(pom + 48);
+    prawa = prawa - pom*i;
+    k++;
+  }
+
+  if (x < 0) putchar('-');
+  for (i = 0; i < j; i++) putchar(L[i]);
+  putchar('.');
+  for (i = 0; i < k; i++) putchar(P[i]);
+  putchar('\n');
+
+  return 0;
+}
